3a3a41f9bdd89ba0dbd7bad07ad1c6ef0f913c8f
zollty <zollty@163.com>
2015-08-10 14:39:13 +0000

    update demo

--- examples/BasicExample.java
+++ examples/BasicExample.java
@@ -9,42 +9,40 @@ import org.zollty.log.Logger;
  */
 public class BasicExample {
 
-	private static final Logger LOG = LogFactory.getLogger(); // 简洁的定义方式
-	
-	public static void main(String[] args) {
-		LOG.debug("===========debug Level=============");
-		LOG.info( "===========info  Level=============");
-		LOG.warn("============warn中文测试============");
-		LOG.error("===========error Level=============");
-		
-		LOG.error("hello {}, now time is {}. ", "guys", new java.util.Date());
-		
-		if( LogFactory.isEnabledFor(ORDER_QUERY) ){
-			try{
-				writeOrderInfoToFile("OrderInfo-Mock-Data");
-			} catch (RuntimeException e) {
-				LOG.error("[{}] [orderNo={}] {}", "Code", "orderNo", e.getMessage());
-			}
-		}
-		
-		try{
-			doService("OrderInfo-Mock-Data");
-		} catch (RuntimeException e) {
-			if( LogFactory.isDebugEnabled() ) {
-				LOG.error(e, "Order reservation errors, orderNo is {}, detail exception info: ", "orderNo");
-			}
-		}
-		
-	}
-	
-	private static void writeOrderInfoToFile(String args) {
-		// TODO
-	}
-	
-	private static void doService(String args) {
-		// TODO
-	}
+    private static final Logger LOG = LogFactory.getLogger(); // 简洁的定义方式
 
-	public static final String ORDER_QUERY="ORDER_QUERY"; //这个为key，其value可以在数据库中配置（false或者true）
+    public static void main(String[] args) {
+        LOG.debug("===========debug Level=============");
+        LOG.info( "===========info  Level=============");
+        LOG.warn("============warn中文测试============");
+        LOG.error("===========error Level=============");
 
+		// 支持参数的占位符
+        LOG.error("hello {}, now time is {}. ", "guys", new java.util.Date());
+
+        if( LOG.isDebugEnabled() ){
+            try{
+                writeOrderInfoToFile("OrderInfo-Mock-Data");
+            } catch (RuntimeException e) {
+                LOG.error("[{}] [orderNo={}] {}", "0x0001", "HQ3SD6", e.getMessage());
+            }
+        }
+
+        try{
+            doService("OrderInfo-Mock-Data");
+        } catch (RuntimeException e) {
+            if( LOG.isDebugEnabled() ) {
+                LOG.error(e, "Order reservation errors, orderNo is {}, detail exception info: ", "HQ3SD6");
+            }
+        }
+
+    }
+
+    private static void writeOrderInfoToFile(String args) {
+        // TODO
+    }
+
+    private static void doService(String args) {
+        // TODO
+    }
 }
